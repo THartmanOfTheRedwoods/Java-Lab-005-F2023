1ced1be5056bf70747b792f014d8c118839e01ef
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-03 13:57:59 -0700

    Part 3 ---Operational---
    >>>MODIFICATIONS
    -Game.announceWinner(): return error resolved// winner variable to carry p1.getName() and push return statement out of 'if' tree
    
    >>>DEFICITS
    
    >>>BUGS
    Player this.p2 always rolls zero
    
    >>>TICKETS
    
    >>>THROWS
    C:\Users\Gnomatic\IdeaProjects\Java-Lab-005\src\Game.java
    Warning:(1, 1) Unused import statement
    Warning:(11, 16) Variable 'currrent' is never used

--- src/Game.java
+++ src/Game.java
@@ -6,9 +6,19 @@ public class Game {
     Dice die;
 
     public void play() {
+        Player current = p1;
+        takeTurn(p1);
+        Player currrent = nextPlayer(p1);
+        takeTurn(current);
+        System.out.println(announceWinner());
     }
 
     public Player nextPlayer(Player current) {
+        if(current == this.p1) {
+            return this.p2;
+        } else {
+            return this.p1;
+        }
     }
 
     public void takeTurn(Player player) {
@@ -16,8 +26,22 @@ public class Game {
     }
 
     public String announceWinner() {
+        String winner = "";
+        System.out.printf("%s rolled %d%n%s rolled %d%n", p1.getName(), p1.getScore(), p2.getName(), p2.getScore());
+        if(this.p1.getScore() > this.p2.getScore()) {
+            winner = p1.getName();
+        } else if (this.p2.getScore() > this.p1.getScore()) {
+            winner = p2.getName();
+        }
+        return String.format("%s won", winner);
     }
 
     public static void main(String[] args) {
+    }
+
+    public Game(Player p1, Player p2, Dice die) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.die = die;
     }
 }
